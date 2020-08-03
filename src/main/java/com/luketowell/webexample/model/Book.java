9c725b8292c5994229d6dc7c16f33c277559bf22
Luke Towell <luke.towell@hotmail.com>
2020-08-03 14:45:49 +0100

    Added Ids, Hashes and relationship mappings
    - Added the Ids and the hashes to the book and author objects
    - Added the relationship mappings to the DB objects
    - Added the JoinTable to the Book class

--- src/main/java/com/luketowell/webexample/model/Book.java
+++ src/main/java/com/luketowell/webexample/model/Book.java
@@ -1,11 +1,22 @@
 package com.luketowell.webexample.model;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+
     private String title;
     private String ispn;
-    private Set<Author> Authors;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "authorId"))
+    private Set<Author> authors;
 
     public Book() {
     }
@@ -13,7 +24,15 @@ public class Book {
     public Book(String title, String ispn, Set<Author> authors) {
         this.title = title;
         this.ispn = ispn;
-        Authors = authors;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -33,10 +52,37 @@ public class Book {
     }
 
     public Set<Author> getAuthors() {
-        return Authors;
+        return authors;
     }
 
     public void setAuthors(Set<Author> authors) {
-        Authors = authors;
+        authors = authors;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", ispn='" + ispn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+
 }
