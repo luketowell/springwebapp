b949f56c4363cc9dfb9d66fdace34a0fca0972bf
Luke Towell <luke.towell@hotmail.com>
2020-08-03 11:59:44 +0100

    Initial Commit
    - the initial commit of the web application

---
+++ src/test/java/com/luketowell/webexample/WebexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luketowell.webexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
