b78b85d04adaac1861e0e6d5d9aad776bdee5976
Luke Towell <luke.towell@hotmail.com>
2020-08-04 09:44:35 +0100

    Added in Publisher entities
    - Added in publisher entities and created a publisher repository

---
+++ src/main/java/com/luketowell/webexample/repositories/PublisherRepository.java
@@ -0,0 +1,8 @@
+package com.luketowell.webexample.repositories;
+
+import com.luketowell.webexample.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
